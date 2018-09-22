cffd7e0a08db17ecbe9b85c1dc4334521d10d32b
Nitish Lobo <nitishlobo@gmail.com>
2018-09-22 18:07:30 +1200

    Tidy files and add comments.

--- bowling.cc
+++ bowling.cc
@@ -1,124 +1,106 @@
+/**************************************************************************
+This file implements the main abstractions in the ten pin bowling game.
+***************************************************************************/
 #include "bowling.h"
 
-//TODO delete debug
-#include <iostream>
-
-//Define constants for rows in game_history
-const int kFrameRow = 0;
-const int kBallRow = 1;
-const int kScoreRow = 2;
-
+/* Initialise the game state */
 Bowling::Bowling() {
-	current_b_ = 0;
+	//First frame of the game starts at 0 not 1 and the number of balls intially bowled is 0.
+	balls_bowled_ = 0;
 	current_f_ = 0;
 
 	//21 columns - can have a maximum of 21 balls bowled in a game_history.
 	game_history.resize(21, { -1, -1, -1 });
 }
 
-void Bowling::Ball(int n) {
+/* Record game history and handle game logic for score keeping - spares, strikes, frames, etc. */
+void Bowling::Ball(int pins_knocked) {
 	//3 rows - these are: frame, pins knocked with each bowl and score respectively.
-	game_history[current_b_].frame = current_f_;
-	game_history[current_b_].ball = n;
+	game_history[balls_bowled_].frame = current_f_;
+	game_history[balls_bowled_].pins = pins_knocked;
 
-	if (current_b_ == 0) {
+	if (balls_bowled_ == 0) {
 		//Score for first ball
-		game_history[current_b_].score = n;
+		game_history[balls_bowled_].score = pins_knocked;
 	} else {
 		//Cumulative ball score
-		game_history[current_b_].score = game_history[current_b_ - 1].score + n;
+		game_history[balls_bowled_].score = game_history[balls_bowled_ - 1].score + pins_knocked;
 	}
 
-	if (current_b_ > 1) {
+	if (balls_bowled_ > 1) {
 		//Spare
 		//If previous bowl and the bowl before that are in the same frame and add to 10 and it's not in the last frame.
-		if ((game_history[current_b_ - 1].frame == game_history[current_b_ - 2].frame)
-			&& (game_history[current_b_ - 1].ball + game_history[current_b_ - 2].ball == 10)
-			&& (game_history[current_b_ - 2].frame != 9)) {
+		if ((game_history[balls_bowled_ - 1].frame == game_history[balls_bowled_ - 2].frame)
+			&& (game_history[balls_bowled_ - 1].pins + game_history[balls_bowled_ - 2].pins == 10)
+			&& (game_history[balls_bowled_ - 2].frame != 9)) {
 			//For a spare, add current ball score to previous frame score.
-			game_history[current_b_ - 1].score += n;
+			game_history[balls_bowled_ - 1].score += pins_knocked;
 
 			//Recalculate next ball score
-			game_history[current_b_].score = game_history[current_b_ - 1].score + n;
+			game_history[balls_bowled_].score = game_history[balls_bowled_ - 1].score + pins_knocked;
 		}
 
 		//Strike
 		//If a 10 is bowled and its not in the last frame.
-		if ((game_history[current_b_ - 2].ball == 10) && (game_history[current_b_ - 2].frame != 9)) {
+		if ((game_history[balls_bowled_ - 2].pins == 10) && (game_history[balls_bowled_ - 2].frame != 9)) {
 			//Add the next two ball scores.
-			game_history[current_b_ - 2].score += game_history[current_b_ - 1].ball;
-			game_history[current_b_ - 2].score += game_history[current_b_].ball;
+			game_history[balls_bowled_ - 2].score += game_history[balls_bowled_ - 1].pins;
+			game_history[balls_bowled_ - 2].score += game_history[balls_bowled_].pins;
 
 			//Recalculate the next two ball scores.
-			game_history[current_b_ - 1].score = game_history[current_b_ - 2].score + game_history[current_b_ - 1].ball;
-			game_history[current_b_].score = game_history[current_b_ - 1].score + n;
+			game_history[balls_bowled_ - 1].score = game_history[balls_bowled_ - 2].score + game_history[balls_bowled_ - 1].pins;
+			game_history[balls_bowled_].score = game_history[balls_bowled_ - 1].score + pins_knocked;
 		}
 	}
 
 	//Go to next ball
-	current_b_++;
+	balls_bowled_++;
 	//Go to next frame if it is not the last frame and either:
 	//          if a strike is achieved
 	//          or 2 previous balls are bowled in the same frame
-	if ((current_f_ != 9) && (n == 10)) {
+	if ((current_f_ != 9) && (pins_knocked == 10)) {
 		current_f_++;
 	}
-	if (current_b_ > 1) {
+	if (balls_bowled_ > 1) {
 		if ((current_f_ != 9)
-			&& (game_history[current_b_ - 1].frame == game_history[current_b_ - 2].frame)) {
+			&& (game_history[balls_bowled_ - 1].frame == game_history[balls_bowled_ - 2].frame)) {
 			current_f_++;
 		}
 	}
 }
 
+/* Return the overall game score, regardless of whether the game is finished or still in progress. */
 int Bowling::GetScore() {
-	return game_history[current_b_ - 1].score;
+	return game_history[balls_bowled_ - 1].score;
 }
 
+/* Return the cumulative score from the the start of the game to a particular frame. */
 int Bowling::GetFrameScore(int frame) {
-	/*
-	int index = GetIndexOfMatch(game_history[kFrameRow], frame);
+	/* Get iterator pointing to the required frame in the vector using lambda function (C++11 specific).
 
-	//Frame not found - invalid frame (ie. not 0 to 9) or not enough balls bowled to enter the frame.
-	if (index == -1) {
-		return -1;
-	}
+		[&frame_to_match = frame] binds the variable frame from the calling scope to
+		a local reference in the lambda, called frame_to_match.
 
-	return game_history[kScoreRow][index + GetBallsBowledInFrame(frame) - 1];
+		(const BallRolled &b) -> bool defines the signature of the lambda,
+		i.e. take one input parameter, b, and return a bool.
 
-	//int index = GetIndexOfMatch(game_history, frame);
-
-	//Frame not found - invalid frame (ie. not 0 to 9) or not enough balls bowled to enter the frame.
-	if (index == -1) {
-		return -1;
-	}
-    */
-	//return game_history[index + GetBallsBowledInFrame(frame) - 1].score;
-    //Get an iterator pointing to the required n in the vector.
+		{ return b.frame == frame_to_match; } is the body of the lambda function.
+		Returns true if b.frame and frame_to_match compare as equal.
+	*/
     auto iterator = std::find_if(game_history.begin(),
                                 game_history.end(),
                                 [&frame_to_match = frame]
                                 (const BallRolled &b) -> bool { return b.frame == frame_to_match; });
 
-    while (iterator->frame == frame) {
+	//Get the score from the last ball bowled in the frame.
+    while (std::next(iterator, 1)->frame == frame) {
         std::advance(iterator, 1);
     }
-    std::advance(iterator, -1);
-
     return iterator->score;
 }
 
+/* Return the number of pins knocked over for a particular ball. */
 int Bowling::GetBallScore(int frame, int ball)  {
-	/*
-	int index = GetIndexOfMatch(game_history[kFrameRow], frame);
-
-	//Frame not found - invalid frame (ie. not 0 to 9) or not enough balls bowled to enter the frame.
-	if (index == -1) {
-		return -1;
-	}
-
-	return game_history[index + ball].ball;
-	*/
     //Get an iterator pointing to the required n in the vector.
 	auto iterator = std::find_if(game_history.begin(),
                                 game_history.end(),
@@ -127,95 +109,41 @@ int Bowling::GetBallScore(int frame, int ball)  {
 
     //Increment the iterator by the number of balls.
     std::advance(iterator, ball);
-
-    return iterator->ball;
+    return iterator->pins;
 }
 
+/* Return the number of frames completed (including the one currently being completed) in the game. */
 int Bowling::GetFramesBowled() {
-	return game_history[current_b_ - 1].frame + 1;
+	return game_history[balls_bowled_ - 1].frame + 1;
 }
 
+/* Return the number of balls bowled for a particular frame.
+	Example: a strike means only 1 ball is bowled for the frame.
+			Anything else means 2 balls are bowled in the frame,
+			unless it is the last frame or a frame is not completed. */
 int Bowling::GetBallsBowledInFrame(int frame) {
-	/*
-	return std::count(game_history[kFrameRow].begin(), game_history[kFrameRow].end(), frame);
-	*/
-	/*int balls = 0;
-	for (BallRolled &i : game_history) {
-		if (i.frame == frame) {
-			balls++;
-		}
-	}
-	return balls;
-	*/
 	return std::count_if(game_history.begin(),
         game_history.end(),
         [&frame_to_match = frame]
         (const BallRolled &b) -> bool { return b.frame == frame_to_match; });
 }
 
+/* Return boolean indicating whether the game has been completed. */
 bool Bowling::IsGameOver() {
 	bool over = false;
 
-	if (current_b_ > 2) {
-		//game_history is over if the last three frames recorded are 9 (ie. 10th frame).
-		if ((game_history[current_b_ - 1].frame == 9) && (game_history[current_b_ - 2].frame == 9) && (game_history[current_b_ - 3].frame == 9)) {
+	if (balls_bowled_ > 2) {
+		//game_history is over if the last three frames recorded are 9 (i.e. 10th frame).
+		if ((game_history[balls_bowled_ - 1].frame == 9) && (game_history[balls_bowled_ - 2].frame == 9) && (game_history[balls_bowled_ - 3].frame == 9)) {
 			over = true;
-		} else if ((game_history[current_b_ - 1].frame == 9) && (game_history[current_b_ - 2].frame == 9)) {
+		} else if ((game_history[balls_bowled_ - 1].frame == 9) && (game_history[balls_bowled_ - 2].frame == 9)) {
 			//game_history is also over if the last two frames are recorded are 9 and
 			//		there are no spares
 			//		nor strikes
-			if (((game_history[current_b_ - 1].frame != 10) && (game_history[current_b_ - 2].frame != 10)) || (game_history[current_b_ - 1].frame + game_history[current_b_ - 2].frame != 10)) {
+			if (((game_history[balls_bowled_ - 1].frame != 10) && (game_history[balls_bowled_ - 2].frame != 10)) || (game_history[balls_bowled_ - 1].frame + game_history[balls_bowled_ - 2].frame != 10)) {
 				over = true;
 			}
 		}
 	}
 	return over;
-}
-
-bool areNumbersEqual(int i, int n) {
-	if (i == n) {
-		return true;
-	}
-	return false;
-}
-
-/*
-int GetIndexOfMatch(std::vector<BallRolled> history, int n) {
-    //Get an iterator pointing to the required n in the vector.
-	auto iterator = std::find_if(history.begin(),
-                                history.end(),
-                                [&n = n]
-                                (const BallRolled &b) -> bool { return ;
-
-	//n not found.
-	if (iterator == v.end()) {
-		return -1;
-	}
-	return std::distance(v.begin(), iterator);
-	return 0;
-}
-*/
-
-/*
-//Returns the index to the first element in which a match to n is found in v.
-int GetIndexOfMatch(std::vector<int> v, int n) {
-	//Get an iterator pointing to the required n in the vector.
-	auto iterator = std::find(v.begin(), v.end(), n);
-
-	//n not found.
-	if (iterator == v.end()) {
-		return -1;
-	}
-	return std::distance(v.begin(), iterator);
-}
-*/
-
-//TODO delete debug
-void PrintVector(std::vector<std::vector<int>> v) {
-	for (std::vector<int>::size_type i = 0; i < 3; i++) {
-		for (std::vector<int>::size_type j = 0; j < 21; j++) {
-			std::cout << v[i][j] << " ";
-		}
-		std::cout << "\n";
-	}
 }
--- bowling.h
+++ bowling.h
@@ -1,22 +1,70 @@
-#include <vector>
-#include <algorithm>	//std::find and std::count
-#include <iterator>		//std::distance
-//#include <boost/bind.hpp>
+/**************************************************************************
+This file declares the two main abstractions in the ten pin bowling game.
+The Bowling class contains the main game logic and it uses the BallRolled
+struct to record the game history.
+***************************************************************************/
 
+#include <vector>
+#include <algorithm>    //std::find and std::count
+#include <iterator>     //std::distance, std::next and std::advance
+
+/* Record game data whenever a ball is bowled.
+    frame - the frame number (0 - 9)
+    pins - number of pins knocked down during a particular bowl (0 - 9)
+    score - cumulative score since the beginning of the game up to a particular roll (0 - 300)
+			Note that the score is per ball roll not per frame. This means that
+			if there are two rolls in a frame each roll will have a different score.
+
+	Example: std::vector<BallRolled> game_history;
+			BallRolled current_roll;
+			current_roll.frame = 0;
+			current_roll.pins = 8;
+			current_roll.score = 8;
+			game_history.push_back(current_roll);
+*/
 struct BallRolled {
 	int frame;
-	int ball;
+	int pins;
 	int score;
 };
 
+
+/*	Record the full game history (it handles all the game logic automatically).
+	Need only one instantiation of this class per player per game.
+
+	Example: Bowli	ng player_nitish;
+			player_nitish.Ball(10);
+*/
 class Bowling {
+/* Class members:
+	current_f_ - current frame. The frame into which the player will be bowling into next (0 - 9)
+	balls_bowled - total number of balls bowled since the beginning of the game (0 - 9)
+	game_history - full history of all the pins knocked on every ball roll and the cumulative game score
+*/
 private:
-	int current_f_, current_b_;			//Current ball and current frame
+	int current_f_;
+	int balls_bowled_;
 	std::vector<BallRolled> game_history;
 
 public:
+/*
+Main class methods:
+	Bowling - class initialisation
+	Ball - call this method everytime a ball is bowled
+*/
 	Bowling();
-	void Ball(int n);
+	void Ball(int pins_knocked);
+
+/*
+Public APIs:
+	GetScore - request the overall game score to date
+	GetFrameScore - request the score since the beginning of the game to the end of a specified frame
+	GetBallScore - request the score since the beginning of the game to a
+					specific ball role in a specific frame
+	GetFramesBowled - request the number of frames played in the game to date (including unfinished frames)
+	GetBallsBowledInFrame - request the number of balls bowled in a specific frame of the game
+	IsGameOver - request to see if the player has completed all 10 frames
+*/
 	int GetScore();
 	int GetFrameScore(int frame);
 	int GetBallScore(int frame, int ball);
@@ -24,10 +72,3 @@ public:
 	int GetBallsBowledInFrame(int frame);
 	bool IsGameOver();
 };
-
-//Returns the index to the first element in which a match to n is found.
-//int GetIndexOfMatch(std::vector<int> v, int n);
-//int GetIndexOfMatch(std::vector<BallRolled> & history, int n);
-
-//TODO delete debug
-void PrintVector(std::vector<std::vector<int>> v);
